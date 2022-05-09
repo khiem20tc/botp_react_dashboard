da27b15dfd85c676b7156fa00ff273f7aa88cc8a
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-10 02:01:18 +0700

    update: action and const

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,22 +2,22 @@ import { UserAction } from "constants/actions";
 import UserRepository from "repositories/user";
 
 const signInAction = (username, password) => ({
-  pendingActionType: UserAction.PENDING_SIGN_IN,
+  type: UserAction.PENDING_SIGN_IN,
   pendingAction: async () => await UserRepository.signIn(username, password),
 });
 
 const signUpAction = (username, password) => ({
-  pendingActionType: UserAction.PENDING_SIGN_UP,
+  type: UserAction.PENDING_SIGN_UP,
   pendingAction: async () => await UserRepository.signUp(username, password),
 });
 
 const signOutAction = () => ({
-  pendingActionType: UserAction.PENDING_SIGN_OUT,
+  type: UserAction.PENDING_SIGN_OUT,
   pendingAction: async () => await UserRepository.signOut(),
 });
 
 const getUserInfoAction = () => ({
-  pendingActionType: UserAction.PENDING_GET_USER_INFO,
+  type: UserAction.PENDING_GET_USER_INFO,
   pendingAction: async () => await UserRepository.getUserInfo(),
 });
 
