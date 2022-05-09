847d9bbb26a28aefdd1f2605cf70d3ee5c5c7a88
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-10 02:00:59 +0700

    feat: staticstic actions

---
+++ src/actions/statistic.js
@@ -0,0 +1,15 @@
+const { StatisticAction } = require("constants/actions");
+const { default: StatisticRepository } = require("repositories/statistic");
+
+const getGeneralStatisticAction = () => ({
+  type: StatisticAction.PENDING_GET_GENERAL_STATISITC,
+  pendingAction: async () => await StatisticRepository.getGeneralStatistic(),
+});
+
+const getDailyStatisticAction = (agentAddress, timeRangesArr) => ({
+  type: StatisticAction.PENDING_GET_DAILY_STATISTIC,
+  pendingAction: async () =>
+    await StatisticRepository.getDailyStatistic(agentAddress, timeRangesArr),
+});
+
+export { getGeneralStatisticAction, getDailyStatisticAction };
