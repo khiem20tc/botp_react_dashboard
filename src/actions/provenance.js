ac135289e01d0bef3644c1c1853c194ceec0a370
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-10 02:24:48 +0700

    feat: provenance action

---
+++ src/actions/provenance.js
@@ -0,0 +1,22 @@
+import { ProvenanceAction } from "constants/actions";
+import ProvenanceRepository from "repositories/provenance";
+
+const getEventBroadcastAction = (agentAddress, userAddress, transactionId) => ({
+  type: ProvenanceAction.PENDING_GET_EVENT_BROADCAST,
+  pendingAction: ProvenanceRepository.getEventBroadcast(
+    agentAddress,
+    userAddress,
+    transactionId
+  ),
+});
+
+const getEventHistoryAction = (agentAddress, userAddress, transactionId) => ({
+  type: ProvenanceAction.PENDING_GET_EVENT_HISTORY,
+  pendingAction: ProvenanceRepository.getEventHistory(
+    agentAddress,
+    userAddress,
+    transactionId
+  ),
+});
+
+export { getEventBroadcastAction, getEventHistoryAction };
