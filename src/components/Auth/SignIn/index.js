3e782f26e85a05698d91f2d5a30b4999b87cb187
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-10 11:54:24 +0700

    eslint airbnb config

--- src/components/Auth/SignIn/index.js
+++ src/components/Auth/SignIn/index.js
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
+
 function SignIn() {
   return SignInView();
 }
 
 function SignInView() {
+  const [someVar, setSomeVar] = useState();
+  useEffect(() => {
+    setSomeVar(1);
+  }, []);
+
   return (
     <>
       <div>Sign in view</div>
