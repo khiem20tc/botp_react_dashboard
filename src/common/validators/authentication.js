33c765c1f499434c5373437c851b9dae66fc38ce
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-11 17:50:41 +0700

    update: wrong password validator

--- src/common/validators/authentication.js
+++ src/common/validators/authentication.js
@@ -1,7 +1,7 @@
 const usernameValidator = (userName) =>
   !userName
     ? "Missing username."
-    : typeof password !== "string"
+    : typeof userName !== "string"
     ? "Wrong type."
     : null;
 
@@ -11,9 +11,9 @@ const passwordValidator = (password) =>
     : typeof password !== "string"
     ? "Wrong type."
     : password.length < 6
-    ? "Minimum password length is 6"
+    ? "Minimum password length is 6."
     : password.match(
-        /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*?[`~!@#$%^&*()\-_=+[{]}\\\|;:'",<.>\/\?]).{6,*}$/
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[`~!@#$%^&*()\-_=+[{\]}\\|;:'",<.>/?]).{6,}$/
       )
     ? null
     : "Password must have a lowercase, uppercase, digits and special character.";
--- src/components/Auth/SignIn/index.js
+++ src/components/Auth/SignIn/index.js
@@ -1,20 +1,62 @@
-import { useEffect, useState } from "react";
+import {
+  FormControl,
+  FormHelperText,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { VisibilityOff, Visibility } from "@mui/icons-material";
+import {
+  passwordValidator,
+  usernameValidator,
+} from "common/validators/authentication";
+import { UserInputAction } from "constants/hooks";
+import useInput from "hooks/useInput";
+import { useState } from "react";
 
 function SignIn() {
   return SignInView();
 }
 
 function SignInView() {
-  const [someVar, setSomeVar] = useState();
-  useEffect(() => {
-    if (someVar) {
-      alert("hehe");
-    }
-  }, []);
+  const [password, dispatchPassword] = useInput(passwordValidator);
+  const [isShowingPassword, setIsShowingPassowrd] = useState(false);
 
   return (
     <>
-      <div>Sign in view</div>
+      <FormControl sx={{ m: 1, width: "25ch" }} variant="filled">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={isShowingPassword ? "text" : "password"}
+          value={password.value}
+          onChange={(e) =>
+            dispatchPassword({
+              type: UserInputAction.ON_CHANGE,
+              value: e.target.value,
+            })
+          }
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setIsShowingPassowrd(!isShowingPassword)}
+                onMouseDown={(e) => e.preventDefault()}
+                edge="end"
+              >
+                {isShowingPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+        <FormHelperText id="outlined-adornment-password-text">
+          {password.error}
+        </FormHelperText>
+      </FormControl>
     </>
   );
 }
