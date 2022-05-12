23dcfdd06514fab72799d3e3244ab1da395630c4
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-13 00:27:19 +0700

    update: kyc validator

--- src/common/validators/kyc.js
+++ src/common/validators/kyc.js
@@ -1,10 +1,18 @@
 const nameValidator = (name) =>
-  !name ? "Missing name" : typeof name !== "string" ? "Wrong type." : null;
+  !name
+    ? "Missing name"
+    : typeof name !== "string"
+    ? "Wrong type."
+    : name.length > 50
+    ? "Maximum name length is 50"
+    : null;
 const descriptionValidator = (description) =>
   !description
     ? "Missing description"
     : typeof description !== "string"
     ? "Wrong type."
+    : description.length > 200
+    ? "Maximum description length is 200"
     : null;
 
 export { nameValidator, descriptionValidator };
