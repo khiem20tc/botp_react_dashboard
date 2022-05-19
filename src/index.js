385dda27e095eb9a86d4e3b48df44c77d85dc2ec
nngthinh <55427371+nngthinh@users.noreply.github.com>
2022-05-19 19:52:33 +0700

    feat: color theme

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import appTheme from "common/theme/theme";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
@@ -6,13 +9,16 @@ import App from "./App";
 import "./index.css";
 
 const store = customCreateStore();
+const theme = createTheme(appTheme);
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
